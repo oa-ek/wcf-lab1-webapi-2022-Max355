f4b2f32f94356ab989cd0ffdf2aca6f527e8ab7b
Max Yevtushok <73525840+Max355@users.noreply.github.com>
2022-11-30 20:44:18 +0200

    connection and documentation

---
+++ DeviceShop/DeviceApi/Controllers/DevicesController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using DeviceShop.Core;
+using DeviceShop.Repos.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeviceApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : Controller
+    {
+        private readonly DeviceShopContext  _context;
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IMapper _mapper;
+
+        public DevicesController(DeviceShopContext context, IWebHostEnvironment webHostEnvironment, IMapper mapper)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+            _mapper = mapper;
+        }
+        /// <summary>
+        /// returns list of devices
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IEnumerable<DevicesDto>> GetActorsAsync()
+        {
+            return _mapper.Map<IEnumerable<DevicesDto>>(_context.Devices.ToList());
+        }
+
+    }
+}
--- DeviceShop/DeviceApi/Program.cs
+++ DeviceShop/DeviceApi/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using DeviceShop.Core;
 using DeviceShop.Repos;
+using DeviceApi;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "Version 1",
+        Title = "Device API",
+        Description = "API Project"
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+var connectionString = builder.Configuration.GetConnectionString("DeviceShopConnection");
+builder.Services.AddDbContext<DeviceShopContext>(options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddAutoMapper(typeof(AppAutoMapper).Assembly);
 
 var app = builder.Build();
 
